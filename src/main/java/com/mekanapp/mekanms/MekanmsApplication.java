9bc9b35b93b63d2ec8fe07da90f5d4a8882f87e1
Mehmet Baran Özdeniz <Ozdenizm6@gmail.com>
2024-01-30 20:11:39 +0300

    second commit

---
+++ src/main/java/com/mekanapp/mekanms/MekanmsApplication.java
@@ -0,0 +1,13 @@
+package com.mekanapp.mekanms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MekanmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MekanmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mekanapp/mekanms/MekanmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mekanapp.mekanms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MekanmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
